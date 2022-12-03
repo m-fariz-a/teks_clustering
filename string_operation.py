ca92206386930656331f52ccaa4e8718b62d5d62
m-fariz-a <falasyir@yahoo.com>
2022-12-03 13:22:07 +0700

    scrapping general word

---
+++ string_operation.py
@@ -0,0 +1,101 @@
+from nltk.stem import PorterStemmer
+from nltk.tokenize import word_tokenize
+from translate import Translator
+
+class StringOperation:
+    
+    def remove_nonalphabet(series_text):
+        """
+        Remove non alphabet from a text series (number and other character)
+
+        parameter
+        ----------
+        seriest_text : Series
+            text input with series dtype
+
+        return
+        ------
+        series_text_output : Series
+            output result
+        """
+
+        series_text_output = series_text.copy()
+        series_text_output = series_text_output.str.lower()
+
+        # remove non alphabet character
+        series_text_output = series_text_output.str.replace(r'[^a-z ]+', '', regex=True)
+
+        return series_text_output
+    
+    
+    
+    def remove_stopwords(series_text, stopword_list):
+        """
+        remove stopword from text
+        
+        parameter
+        ----------
+        seriest_text : Series
+            text input with series dtype
+        stopword_list : list
+            list of stopwords
+
+        return
+        ------
+        series_text_output : Series
+            output result
+        """
+        
+        series_text_output = series_text.apply(lambda x: ' '.join([word for word in x.split() if word not in (stopword_list)]))
+        
+        return series_text_output
+    
+    
+    
+    def word_stemming(series_text):
+        """
+        get a standard word
+        
+        parameter
+        ----------
+        seriest_text : Series
+            text input with series dtype
+
+        return
+        ------
+        series_text_output : Series
+            output result
+        """
+        
+        stemmer = PorterStemmer()
+        tokenizer = word_tokenize()
+        
+        series_text_output = series_text.apply(lambda x: ' '.join([stemmer.stem(word) for word in tokenizer(x)]))
+        
+        return series_text_output
+    
+    
+    
+    def translate_id_to_en(series_text,
+                          src_lang='id', dest_lang='en'):    
+        """
+        get translate into specific language
+        
+        parameter
+        ----------
+        seriest_text : Series
+            text input with series dtype
+        trasnlator : function
+            text input with series dtype
+
+        return
+        ------
+        series_text_output : Series
+            output result
+        """
+        
+        translator= Translator(from_lang=src_lang, to_lang=dest_lang)
+        
+        series_text_output = series_text.apply(lambda x: translator.translate(x))
+        
+        return series_text_output
